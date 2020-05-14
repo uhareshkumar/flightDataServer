9c5172cf1ae4ab28162017b0677fc78f3b713625
uhareshkumar <uharesh.kumar2015@vit.ac.in>
2020-05-14 22:56:37 +0530

    CORS error fixed

--- src/index.js
+++ src/index.js
@@ -1,16 +1,13 @@
 const express = require('express');
 const bodyParser =  require('body-parser');  
-
+const cors = require('cors');
 const getFlightsRouter = require('./router/getFlights');
 
 const app = express();
 app.use(bodyParser.json());
+app.use(cors());
 app.use(getFlightsRouter);
 
-app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', '*');
-    next();
-});
 
 app.listen(3001, () =>  { 
     console.log('server running on port 3001'); 
